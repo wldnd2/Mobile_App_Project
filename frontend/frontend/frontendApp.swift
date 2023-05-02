da157ccc81ec8e30e5aa57c16b7c5e5e4f9d2681
wldnd2 <jiwoong000628@gmail.com>
2023-05-02 22:11:44 +0900

    Add frontend file

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by 전지웅 on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
