037153da8fd3578b21ef5aab51f52e19b57fbb98
wldnd2 <jiwoong000628@gmail.com>
2023-05-08 16:16:36 +0900

    Feat: Repository별 Insert기능 테스트 구현

---
+++ backend/src/test/java/com/example/backend/repository/BoardCommentRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.example.backend.repository;
+
+import com.example.backend.domain.Board;
+import com.example.backend.domain.BoardComment;
+import com.example.backend.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.sql.Timestamp;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Transactional
+public class BoardCommentRepositoryTests {
+    @Autowired
+    BoardCommentRepository boardCommentRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    BoardRepository boardRepository;
+    @Test
+    public void InsertTest() {
+        Long userId = 1L;
+        Optional<User> userResult = userRepository.findById(userId);
+        Long boardId = 1L;
+        Optional<Board> boardResult = boardRepository.findById(boardId);
+        if(userResult.isPresent() && boardResult.isPresent()) {
+            IntStream.rangeClosed(1, 10).forEach(i -> {
+                BoardComment boardComment = BoardComment.builder()
+                        .commentContent("comment ... " + i)
+                        .commentDate(new Timestamp(System.currentTimeMillis()))
+                        .user(userResult.get())
+                        .board(boardResult.get())
+                        .build();
+                boardCommentRepository.save(boardComment);
+            });
+        }
+    }
+}
