7b03f815b7904d0ba930ad1ab3252034ed599aac
min1018 <meet214ba@naver.com>
2023-05-08 22:22:27 +0900

    Feat: NewHomeSlideView추가

--- frontend/frontend/Views/ContentView.swift
+++ frontend/frontend/Views/ContentView.swift
@@ -8,19 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+      NewHomeSlideView()
     }
-}
+  }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
