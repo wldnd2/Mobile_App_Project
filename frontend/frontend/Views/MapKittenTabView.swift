a63a8aea10d75bfae7664cbe91016301e11c6dd5
min1018 <meet214ba@naver.com>
2023-05-11 09:19:55 +0900

    Feat: MapSlideTabView 추가

---
+++ frontend/frontend/Views/MapKittenTabView.swift
@@ -0,0 +1,105 @@
+//
+//  MapKittenTabView.swift
+//  frontend
+//
+//  Created by 이정민 on 2023/05/10.
+//
+
+import SwiftUI
+
+struct MapKittenTabView: View {
+  @State private var isLiked = false
+  @State private var count: Int = 263
+  
+  var body: some View{
+    HStack(alignment: .top, spacing: 10){
+      Image("고양이2L")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 115, height: 115)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+      VStack(alignment: .leading, spacing: 0){
+        Text("냥이가 저를 쳐다봐요 운명인 것 같아요!!")
+          .font(.footnote)
+          .bold()
+          
+        
+        Spacer()
+        
+        LikesComments
+      }
+      .padding(.top,10)
+      
+      
+      
+    }.frame(width: 500, height: 100)
+      .padding(.trailing, 50)
+  }
+  
+}
+
+private extension MapKittenTabView {
+  
+  var LikesComments: some View{
+    HStack{
+        HeartButton(isLiked: $isLiked, count:$count)
+      
+        Image(systemName: "text.bubble")
+          .resizable()
+          .frame(width: 20, height: 20)
+          
+          Text("32")
+            .font(.footnote)
+          
+        }.padding(.bottom, 10)
+  }
+}
+
+struct HeartButton: View{
+  @Binding var isLiked : Bool
+  @Binding var count : Int
+  
+  private let animationDuration: Double = 0.1
+  private var animationScale: CGFloat {
+    isLiked ? 0.7 : 1.3
+  }
+  @State private var animate = false
+  var body: some View{
+    Button( action: {
+      if isLiked == false {
+        self.count += 1
+      }
+      else {
+              self.count -= 1
+            }
+              
+      self.isLiked.toggle()
+      self.animate = true
+            
+      DispatchQueue.main.asyncAfter(deadline:.now() + self.animationDuration, execute: {
+              self.animate = false
+              
+            })
+      }, label: {
+          Image(systemName: isLiked ? "heart.fill" : "heart")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 18)
+            .foregroundColor(isLiked ? .red: .gray)
+          
+          Text(" \(count) ")
+            .font(.footnote)
+            .foregroundColor(.black)
+          })
+          .scaleEffect(animate ? animationScale : 1)
+          .animation(.easeIn(duration: animationDuration))
+        }
+      }
+
+
+struct MapKittenTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapKittenTabView()
+    }
+}
