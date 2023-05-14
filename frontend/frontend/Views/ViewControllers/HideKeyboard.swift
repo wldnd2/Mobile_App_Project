120e21d4c42074edc9c5bb8adaeb0e82b4003309
NOH_ <noyusu98@gmail.com>
2023-05-15 02:21:27 +0900

    Feat: diaryWriteUI, strayWriteUI 구현

--- frontend/frontend/Views/ViewControllers/HideKeyboard.swift
+++ frontend/frontend/Views/ViewControllers/HideKeyboard.swift
@@ -4,11 +4,11 @@ import SwiftUI
 extension UIApplication {
     func hideKeyboard() {
         guard let windowScene = connectedScenes.first as? UIWindowScene,
-              let window = windowScene.windows.first else { return }
-        let tapRecognizer = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+              let win = windowScene.windows.first else { return }
+        let tapRecognizer = UITapGestureRecognizer(target: win, action: #selector(UIView.endEditing))
         tapRecognizer.cancelsTouchesInView = false
         tapRecognizer.delegate = self
-        window.addGestureRecognizer(tapRecognizer)
+        win.addGestureRecognizer(tapRecognizer)
     }
 }
 
