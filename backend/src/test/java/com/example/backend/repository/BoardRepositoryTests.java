062bb3599ca3af4c9f0c7d49bd0ece732934a47c
wldnd2 <jiwoong000628@gmail.com>
2023-05-21 16:26:51 +0900

    Refactor: entity수정으로 인한 Test코드 수정

--- backend/src/test/java/com/example/backend/repository/BoardRepositoryTests.java
+++ backend/src/test/java/com/example/backend/repository/BoardRepositoryTests.java
@@ -2,19 +2,15 @@ package com.example.backend.repository;
 
 import com.example.backend.board.domain.Board;
 import com.example.backend.board.repository.BoardRepository;
-import com.example.backend.user.domain.User;
 import com.example.backend.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.transaction.Transactional;
 import java.sql.Timestamp;
-import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
-@Transactional
 public class BoardRepositoryTests {
     @Autowired
     BoardRepository boardRepository;
@@ -23,20 +19,16 @@ public class BoardRepositoryTests {
 
     @Test
     public void InsertTest() {
-        Long userId = 1L;
-        Optional<User> result = userRepository.findById(userId);
-        if(result.isPresent()) {
-            IntStream.rangeClosed(1, 10).forEach(i -> {
-                Board board = Board.builder()
-                        .boardTitle("Board_" + i)
-                        .boardImg("Cat_img_" + i)
-                        .boardContent("Board_content_" + i)
-                        .boardLike(i)
-                        .boardDate(new Timestamp(System.currentTimeMillis()))
-                        .user(result.get())
-                        .build();
-                boardRepository.save(board);
-            });
-        }
+        IntStream.rangeClosed(1, 3).forEach(i -> {
+            Board board = Board.builder()
+                    .boardWriter("Google_"+i)
+                    .boardTitle("Board_" + i)
+                    .boardImg("Cat_img_" + i)
+                    .boardContent("Board_content_" + i)
+                    .boardLike(i)
+                    .boardDate(new Timestamp(System.currentTimeMillis()))
+                    .build();
+            boardRepository.save(board);
+        });
     }
 }
