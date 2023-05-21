26fc24799897169f676cc1e8653177f4630e833b
wldnd2 <jiwoong000628@gmail.com>
2023-05-21 16:28:19 +0900

    Feat: DB 조회 쿼리문 작성

--- backend/src/main/java/com/example/backend/board/repository/BoardRepository.java
+++ backend/src/main/java/com/example/backend/board/repository/BoardRepository.java
@@ -3,6 +3,12 @@ package com.example.backend.board.repository;
 
 import com.example.backend.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query(value = "select * from board where board_writer = :writer", nativeQuery=true)
+    List<Board> showUserBoardPara(@Param("writer") String writer);
 }
