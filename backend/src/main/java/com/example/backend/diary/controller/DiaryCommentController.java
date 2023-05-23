00a43b6381ca93d33ff1843f6f603a76a7abf137
flora101 <flora8207@naver.com>
2023-05-23 20:49:50 +0900

    Feat: Diary 댓글 조회 구현

--- backend/src/main/java/com/example/backend/diary/controller/DiaryCommentController.java
+++ backend/src/main/java/com/example/backend/diary/controller/DiaryCommentController.java
@@ -1,4 +1,23 @@
 package com.example.backend.diary.controller;
 
+import com.example.backend.board.domain.BoardComment;
+import com.example.backend.board.service.BoardCommentService;
+import com.example.backend.diary.domain.DiaryComment;
+import com.example.backend.diary.service.DiaryCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/diary_comment")
 public class DiaryCommentController {
+    @Autowired
+    DiaryCommentService diaryCommentService;
+
+    @GetMapping("/list")
+    public List<DiaryComment> showComment(@RequestParam(value = "id") String id) { return diaryCommentService.showComment(id); }
+
 }
--- backend/src/main/java/com/example/backend/diary/service/DiaryCommentService.java
+++ backend/src/main/java/com/example/backend/diary/service/DiaryCommentService.java
@@ -1,4 +1,42 @@
 package com.example.backend.diary.service;
 
+import com.example.backend.board.domain.Board;
+import com.example.backend.board.domain.BoardComment;
+import com.example.backend.board.repository.BoardCommentRepository;
+import com.example.backend.board.repository.BoardRepository;
+import com.example.backend.diary.domain.Diary;
+import com.example.backend.diary.domain.DiaryComment;
+import com.example.backend.diary.repository.DiaryCommentRepository;
+import com.example.backend.diary.repository.DiaryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DiaryCommentService {
+    @Autowired
+    DiaryCommentRepository diaryCommentRepository;
+    @Autowired
+    DiaryRepository diaryRepository;
+
+    public List<DiaryComment> showComment(String id) {
+        return diaryCommentRepository.showDiaryCommentPara(id);
+    }
+
+    public DiaryComment createComment(Long id, DiaryComment diaryComment){
+        Optional<Diary> diaryResult = diaryRepository.findById(id);
+        if(diaryResult.isPresent()) {
+            DiaryComment comment = DiaryComment.builder()
+                    .commentWriter(diaryComment.getCommentWriter())
+                    .commentContent(diaryComment.getCommentContent())
+                    .commentDate(new Timestamp(System.currentTimeMillis()))
+                    .diary(diaryResult.get())
+                    .build();
+            return diaryCommentRepository.save(comment);
+        }
+        return null;
+    }
 }
