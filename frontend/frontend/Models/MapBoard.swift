59f9618d576fd57c732c3c2c775e41732716eb05
min1018 <102647148+min1018@users.noreply.github.com>
2023-05-23 20:57:15 +0900

    Feat: 길냥이 게시글 구현

---
+++ frontend/frontend/Models/MapBoard.swift
@@ -0,0 +1,24 @@
+//
+//  MapBoard.swift
+//  frontend
+//
+//  Created by 이정민 on 2023/05/23.
+//
+
+import Foundation
+
+
+struct MapBoard: Hashable, Codable{
+  
+  let communityId: Int
+  let communityWriter: String
+  let communityImg: String
+  let communityContent: String
+  let communityDate: String
+  let communityLike: Int
+  let langtitude: String
+  let longtitude: String
+}
+
+let mapex = MapBoard(communityId: 1, communityWriter: "아이디", communityImg: "img", communityContent: "저 쳐다봐요!! 운명인가봐요", communityDate: "2023-05-08", communityLike: 32, langtitude: "35.886", longtitude: "128.6112")
+
