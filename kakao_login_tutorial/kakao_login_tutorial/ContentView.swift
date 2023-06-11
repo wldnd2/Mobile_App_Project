e8ecf1a9a697e48278e85524ccb3e8ce0065459f
배근영 <94815293+flora101@users.noreply.github.com>
2023-06-11 18:01:50 +0900

    Feat: kakao login 완성

--- kakao_login_tutorial/kakao_login_tutorial/ContentView.swift
+++ kakao_login_tutorial/kakao_login_tutorial/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var kakaoAuthVM : KakaoAuthVM = KakaoAuthVM()
+    
     var body: some View {
         
         VStack(spacing: 20) {
             Text("Hello, world!")
                 .padding()
-            Button("카카오 로그인",action: {})
+            Button("카카오 로그인",action: {
+                kakaoAuthVM.handleKakakoLogin()
+                
+            })
             Button("카카오 로그아웃", action: {})
         }
         
---
+++ kakao_login_tutorial/kakao_login_tutorial/KakaoAuthVM.swift
@@ -0,0 +1,45 @@
+//
+//  KakaoAuthVM.swift
+//  kakao_login_tutorial
+//
+//  Created by 배근영 on 2023/06/11.
+//
+
+import Foundation
+import Combine
+import KakaoSDKAuth
+import KakaoSDKUser
+
+class KakaoAuthVM : ObservableObject{
+    
+    
+    func handleKakakoLogin(){
+        // 카카오톡 실행 가능 여부 확인
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+
+                    //do something
+                    _ = oauthToken
+                }
+            }
+        } else {
+            //설치 안되어있을때-> 카카오 웹뷰로 로그인
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                    if let error = error {
+                        print(error)
+                    }
+                    else {
+                        print("loginWithKakaoAccount() success.")
+
+                        //do something
+                        _ = oauthToken
+                    }
+                }
+        }
+    }
+}
